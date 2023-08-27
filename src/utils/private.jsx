b3241588fda7b3f69c33ca3125176fb16e136ed4
UNovak <urbanwritescode@gmail.com>
2023-08-27 03:27:45 +0200

    Extracted he loading component from private.jsx
    The loading animation is now a separate component

--- src/utils/private.jsx
+++ src/utils/private.jsx
@@ -1,23 +1,13 @@
-import { Navigate, Route, Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar'
 import { Fragment } from 'react'
+import Loading from '../components/loading'
 
 import React from 'react'
 
 const Private = ({ session, status }) => {
   if (status) {
-    return (
-      <Fragment>
-        <div className='d-flex justify-content-center'>
-          <div className='spinner-grow text-success m-2' role='status' />
-          <div className='spinner-grow text-success m-2' role='status' />
-          <div className='spinner-grow text-success m-2' role='status' />
-          <div className='spinner-grow text-success m-2' role='status' />
-          <div className='spinner-grow text-success m-2' role='status' />
-          <div className='spinner-grow text-success m-2' role='status' />
-        </div>
-      </Fragment>
-    )
+    return <Loading />
   }
 
   return session !== null ? (
